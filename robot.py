82d5527d1085d3069ba6401e5a97b9d8ce9525b0
IIoTGroup1 <120136586+IIoTGroup1@users.noreply.github.com>
2022-12-09 09:08:48 -0600

    Update robot.py

--- robot.py
+++ robot.py
@@ -5,6 +5,16 @@ from pyniryo import api
 
 
 
+class Controller:
+    def __init__(self, robot, ip_address):
+        """
+        Constructs the robot controller.
+        """
+        self.robot = robot
+        self.ip_address = ip_address
+        
+
+
 class Robot:
     @staticmethod
     def init(ip_address: str):
@@ -14,3 +24,5 @@ class Robot:
         robot = api.tcp_client.NiryoRobot(ip_address)
         robot.calibrate_auto()
         return robot
+
+    
