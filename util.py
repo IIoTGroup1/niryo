5ed7078e3f525fd58012b56b27ab22e0824dfe2f
IIoTGroup1 <120136586+IIoTGroup1@users.noreply.github.com>
2022-12-09 09:27:09 -0600

    Update util.py

--- util.py
+++ util.py
@@ -18,7 +18,7 @@ def valid_ip(ip_address: str) -> bool:
 
 
 
-footer = """<style>
+footer_og = """<style>
 a:link , a:visited{
 color: blue;
 background-color: transparent;
@@ -45,3 +45,40 @@ text-align: center;
 <p>Developed with ❤ by <a style='display: block; text-align: center;' href="https://www.thingy.dev/" target="_blank">Thingy</a></p>
 </div>
 """
+
+
+
+footer = """<style>
+a:link , a:visited{
+color: blue;
+background-color: transparent;
+text-decoration: underline;
+}
+
+a:hover,  a:active {
+color: red;
+background-color: transparent;
+text-decoration: underline;
+}
+
+.footer {
+position: fixed;
+left: 0;
+bottom: 0;
+width: 100%;
+background-color: #0E1117;
+color: white;
+text-align: center;
+}
+</style>
+<div class="footer">
+<p></p>
+</div>
+"""
+
+
+def update_footer(text: str):
+    """
+    Updates the footer element with the given text.
+    """
+    footer = footer.replace("<p></p>", "<p>"+text+"</p>")
