b55a4aa12907179feab3b3e31c4d5e74ad937500
IIoTGroup1 <120136586+IIoTGroup1@users.noreply.github.com>
2022-12-09 08:56:17 -0600

    Create misc.py

---
+++ misc.py
@@ -0,0 +1,16 @@
+"""
+...
+"""
+
+
+
+
+def valid_ip(ip_address: str) -> bool:
+    """
+    Makes a crude attempt at validating the given IP address.
+    """
+    dots = 0
+    for char in ip_address:
+        if char == '.':
+            dots += 1
+    return True if dots==3 else False
