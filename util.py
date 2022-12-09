2cf2588d8d18daefc3dd6c6811fdb233e334682c
IIoTGroup1 <120136586+IIoTGroup1@users.noreply.github.com>
2022-12-09 09:16:11 -0600

    Update util.py

--- util.py
+++ util.py
@@ -14,3 +14,34 @@ def valid_ip(ip_address: str) -> bool:
         if char == '.':
             dots += 1
     return True if dots==3 else False
+
+
+
+
+footer="""<style>
+a:link , a:visited{
+color: blue;
+background-color: transparent;
+text-decoration: underline;
+}
+
+a:hover,  a:active {
+color: red;
+background-color: transparent;
+text-decoration: underline;
+}
+
+.footer {
+position: fixed;
+left: 0;
+bottom: 0;
+width: 100%;
+background-color: white;
+color: black;
+text-align: center;
+}
+</style>
+<div class="footer">
+<p>Developed with ❤ by <a style='display: block; text-align: center;' href="https://www.thingy.dev/" target="_blank">Thingy</a></p>
+</div>
+"""
