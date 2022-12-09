b0b6397ca71d7c3801da98aa541d5ede1deef53e
IIoTGroup1 <120136586+IIoTGroup1@users.noreply.github.com>
2022-12-09 09:53:33 -0600

    Update util.py

--- util.py
+++ util.py
@@ -1,7 +1,7 @@
 """
 ...
 """
-
+import datetime
 
 
 
@@ -16,97 +16,9 @@ def valid_ip(ip_address: str) -> bool:
     return True if dots==3 else False
 
 
-
-
-footer_og = """<style>
-a:link , a:visited{
-color: blue;
-background-color: transparent;
-text-decoration: underline;
-}
-
-a:hover,  a:active {
-color: red;
-background-color: transparent;
-text-decoration: underline;
-}
-
-.footer {
-position: fixed;
-left: 0;
-bottom: 0;
-width: 100%;
-background-color: #0E1117;
-color: white;
-text-align: center;
-}
-</style>
-<div class="footer">
-<p>Developed with ❤ by <a style='display: block; text-align: center;' href="https://www.thingy.dev/" target="_blank">Thingy</a></p>
-</div>
-"""
-
-
-
-footer = """<style>
-a:link , a:visited{
-color: blue;
-background-color: transparent;
-text-decoration: underline;
-}
-
-a:hover,  a:active {
-color: red;
-background-color: transparent;
-text-decoration: underline;
-}
-
-.footer {
-position: fixed;
-left: 0;
-bottom: 0;
-width: 100%;
-background-color: #0E1117;
-color: white;
-text-align: center;
-}
-</style>
-<div class="footer">
-<p></p>
-</div>
-"""
-
-
-def update_footer(text: str):
+def get_datetime() -> str:
     """
-    Updates the footer element with the given text.
+    Returns the current date & time.
     """
-    footer = """<style>
-    a:link , a:visited{
-    color: blue;
-    background-color: transparent;
-    text-decoration: underline;
-    }
-
-    a:hover,  a:active {
-    color: red;
-    background-color: transparent;
-    text-decoration: underline;
-    }
-
-    .footer {
-    position: fixed;
-    left: 0;
-    bottom: 0;
-    width: 100%;
-    background-color: #0E1117;
-    color: white;
-    text-align: center;
-    }
-    </style>
-    <div class="footer">
-    <p></p>
-    </div>
-    """
-    footer = footer.replace("<p></p>", "<p>"+text+"</p>")
-    return footer
+    now = datetime.datetime.now()
+    return now.strftime("%m-%d-%Y %H:%M:%S")
