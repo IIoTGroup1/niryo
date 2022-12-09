b8929d79869db391f2c95a40c39f4cbdd6ad145e
IIoTGroup1 <120136586+IIoTGroup1@users.noreply.github.com>
2022-12-09 09:32:57 -0600

    Update util.py

--- util.py
+++ util.py
@@ -81,5 +81,32 @@ def update_footer(text: str):
     """
     Updates the footer element with the given text.
     """
+    footer = """<style>
+    a:link , a:visited{
+    color: blue;
+    background-color: transparent;
+    text-decoration: underline;
+    }
+
+    a:hover,  a:active {
+    color: red;
+    background-color: transparent;
+    text-decoration: underline;
+    }
+
+    .footer {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    background-color: #0E1117;
+    color: white;
+    text-align: center;
+    }
+    </style>
+    <div class="footer">
+    <p></p>
+    </div>
+    """
     footer = footer.replace("<p></p>", "<p>"+text+"</p>")
     return footer
