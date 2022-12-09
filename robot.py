612edc0b997d38911d87d2671c90effb077d3bb0
IIoTGroup1 <120136586+IIoTGroup1@users.noreply.github.com>
2022-12-09 08:57:28 -0600

    Update robot.py

--- robot.py
+++ robot.py
@@ -6,9 +6,9 @@ from pyniryo import api
 
 
 class Robot:
-  @staticmethod
-  def init(ip_address: str):
-    """
-    Initializes the robot with the given IP address.
-    """
-    return api.tcp_client.NiryoRobot(ip_address)
+    @staticmethod
+    def init(ip_address: str):
+        """
+        Initializes the robot with the given IP address.
+        """
+        return api.tcp_client.NiryoRobot(ip_address)
